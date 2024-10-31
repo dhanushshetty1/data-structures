67ab2a59adc2f0ecdfea4b53f82c6cfa0b68defa
Dhanush S <88223463+dhanushshetty1@users.noreply.github.com>
2024-10-31 12:42:19 +0530

    Add files via upload

---
+++ array/intro_array/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	//use break points
+	int A[5];
+	int B[5]={1,2,3,4,5};
+	int C[10]={2,3,4};
+	int D[5]={0};
+	int E[]={1,2,3,4,5,6};
+	
+	for(int i=0; i<5; i++)
+	{
+		cout << &A[i] << endl;
+		}
+	return 0;
+	
+	return 0;
+}
